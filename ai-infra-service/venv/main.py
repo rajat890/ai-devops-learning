37a1599dea9435814f145850141f0e3455cfa398
rajat890 <rajatdhari@gmail.com>
2026-02-16 14:50:54 +0530

    learning about FastAPi

---
+++ ai-infra-service/venv/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class DebugRequest(BaseModel):
+    message: str
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "AI Infra Service is running"}
+
+
+@app.post("/debug")
+def debug(request: DebugRequest):
+    return {"received_message": request.message}
+
+#@app.get("/")
+#def home():
+#    return {"message": "AI Infra Service is running"}
+
+#@app.get("/health")
+
+#def health_check():
+#    return {"status": "healthy"}
